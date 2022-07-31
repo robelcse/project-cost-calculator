0d62fcc06a2665a8ebc8301bbf3328269edb6413
robelcse <mdrobel.cse@gmail.com>
2022-07-30 23:02:57 -0700

    project cost calculator

--- resources/views/pdf_result.blade.php
+++ resources/views/pdf_result.blade.php
@@ -33,40 +33,61 @@
                 <img src="{{ public_path('uploads/images/'.$imagename) }}" height="100px" />
             </h3>
             @endif
+
+            @if(!is_null($company_address))
             <p>Company Address: {{ $company_address }}</p>
+            @endif
         </div>
         <div class="client">
+            @if(!is_null($client_name))
             <h3>Client name: {{ $client_name }}</h3>
+            @endif
+            @if(!is_null($client_address))
             <p>Client Address: {{ $client_address }}</p>
+            @endif
 
         </div>
         <div class="table">
             <table>
+                @if(!is_null($frontend_dev))
                 <tr>
                     <td>Frontend Developer : {{ $frontend_dev == null ? 0:$frontend_dev }}</td>
                     <td>Cost per dev: {{ $frontend_dev_cost == null ? 0: $frontend_dev_cost }} $</td>
                 </tr>
+                @endif
+                @if(!is_null($backend_dev))
                 <tr>
                     <td>Backend Developer : {{ $backend_dev == null ? 0:$backend_dev }}</td>
                     <td>Cost per dev: {{ $backend_dev_cost == null ? 0:$backend_dev_cost }} $</td>
                 </tr>
+                @endif
+                @if(!is_null($mobile_app_dev))
                 <tr>
                     <td>Mobile App Developer : {{ $mobile_app_dev == null ? 0:$mobile_app_dev }}</td>
                     <td>Cost per dev: {{ $mobile_app_dev_cost == null ? 0:$mobile_app_dev_cost }} $</td>
                 </tr>
+                @endif
+                @if(!is_null($total_hour_of_work))
                 <tr>
                     <td>Number of hours to do work : {{ $total_hour_of_work == null ? 0:$total_hour_of_work }}</td>
                     <td>Cost per hour: {{ $cost_per_hour == null ? 0:$cost_per_hour }} $</td>
                 </tr>
+                @endif
+                @if(!is_null($server_cost))
                 <tr>
                     <td>Server Cost : {{ $server_cost == null ? 0:$server_cost }} $</td>
                 </tr>
+                @endif
+                @if(!is_null($domain_cost))
                 <tr>
                     <td>Domain Cost : {{ $domain_cost == null ? 0:$domain_cost }} $</td>
                 </tr>
+                @endif
+                @if(!is_null($vat))
                 <tr>
                     <td>Vat : {{ $vat == null ? 0:$vat }} %</td>
                 </tr>
+                @endif
                 @foreach ($arr_of_items as $items)
                 @if(!is_null($items[0]) && !is_null($items[1]))
                 <tr>
